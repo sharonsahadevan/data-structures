11d96c5c2aa3711866384c704ebe3475ebd21ed4
Sharon <sharon@digitalxlabs.com>
2020-11-02 18:30:22 +0530

    BST added

---
+++ BST/BST.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+} *root = NULL;
+
+void insert(int key)
+{
+    struct Node *t = root;
+    struct Node *r, *p;
+
+    if (root == NULL)
+    {
+        p = (struct Node *)malloc(sizeof(struct Node));
+        p->data = key;
+        p->lchild = p->rchild = NULL;
+    }
+}
