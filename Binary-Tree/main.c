5ef298a69e1478cebbdeb7880908257cf385e3c2
Sharon <sharon@digitalxlabs.com>
2020-11-02 12:39:06 +0530

    Binary tree node count,height functions added

--- Binary-Tree/main.c
+++ Binary-Tree/main.c
@@ -132,6 +132,27 @@ void LevelOrder(struct Node *root)
     }
 }
 
+int count(struct Node *root)
+{
+    if (root)
+        return count(root->lchild) + count(root->rchild) + 1;
+    return 0;
+}
+
+int height(struct Node *root)
+{
+    int x, y;
+    if (root == 0)
+        return 0;
+
+    x = height(root->lchild);
+    y = height(root->rchild);
+    if (x > y)
+        return x + 1;
+    else
+        return y + 1;
+}
+
 int main()
 {
     Treecreate();
