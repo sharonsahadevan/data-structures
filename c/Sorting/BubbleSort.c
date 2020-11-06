aa523faa1f860cf5912249c1f60a5ae47e2b67b3
Sharon <sharon@digitalxlabs.com>
2020-11-06 15:17:45 +0530

    Bubble sort

--- c/Sorting/BubbleSort.c
+++ c/Sorting/BubbleSort.c
@@ -10,14 +10,20 @@ void swap(int *x, int *y)
 
 void BubbleSort(int A[], int n)
 {
-    int i, j;
+    int i, j, flag;
     for (i = 0; i < n - 1; i++)
     {
+        flag = 0;
         for (j = 0; j < n - i - 1; j++)
         {
             if (A[j] > A[j + 1])
+            {
                 swap(&A[j], &A[j + 1]);
+                flag = 1;
+            }
         }
+        if (flag == 1)
+            break;
     }
 }
 void main()
